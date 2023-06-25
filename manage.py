30b44a9793490a1c46859cbd76b14b6d02b29e79
Andrew Yoder <ayoder770@gmail.com>
2023-06-25 16:54:00 -0400

    First commit to push app to Render

--- manage.py
+++ manage.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
+#####################################################################
+# File Name: manage.py
+#
+# Description: manage.py file for app
+#
+# File History
+# 06/25/2023 - Andrew Yoder : Added header
+#                           : Update name from gettingstarted to gamedashapp
+######################################################################
+
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamedashapp.settings")
 
     from django.core.management import execute_from_command_line
 
